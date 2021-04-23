de8bab66ffdc986946aebadcc00af4561685b4a7
Admin <rajkumarmvr@gmail.com>
2021-04-23 17:34:33 +0530

    Adding index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var http = require("http");
+http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type':'text/plain'});
+    response.write('Hello World...');
+    response.end('Thanks....')
+}).listen(3000);
+console.log('Server is running...');
