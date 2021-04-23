70b92fb968ff3b8292fae09703ded25dc57a538f
Admin <rajkumarmvr@gmail.com>
2021-04-23 17:40:45 +0530

    index.js message content modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require("http");
 http.createServer(function(request, response){
     response.writeHead(200, {'Content-Type':'text/plain'});
-    response.write('Hello World...');
-    response.end('Thanks....')
+    response.write('Hello my world...');
+    response.end('Thanks for scripting....')
 }).listen(3000);
 console.log('Server is running...');
